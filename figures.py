0dfe9ae157fb3766a7b1d1f38b31f1a65c9b728d
AlexNaguy <gorniynaguy@mail.ru>
2023-04-25 14:57:35 +0300

    Создал и реализовал класс Square

--- figures.py
+++ figures.py
@@ -23,3 +23,23 @@ class Shape:
 
     def info(self):
         print(f"Координаты точки начала: x:{self.__x}, y:{self.__y}")
+
+class Square(Shape, ManagerFigures):
+    __NAME = "Квадрат"
+
+    def __init__(self, x: int, y: int, side: float):
+        super().__init__(x, y)
+        self.__side = side
+
+    @property
+    def side(self):
+        return self.__side
+
+    @side.setter
+    def side(self, side):
+        self.__side = side
+
+    def info(self):
+        super().info()
+        print(f"Данная фигура: {self.__NAME}"
+              f" cо стороной: {self.__side}\n")
