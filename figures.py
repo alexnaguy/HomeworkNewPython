32957d933b2615b5388986a3d5f65877f3ab84a9
AlexNaguy <gorniynaguy@mail.ru>
2023-04-25 14:58:50 +0300

    Создал и реализовал класс Rectangle

--- figures.py
+++ figures.py
@@ -43,3 +43,33 @@ class Square(Shape, ManagerFigures):
         super().info()
         print(f"Данная фигура: {self.__NAME}"
               f" cо стороной: {self.__side}\n")
+
+class Rectangle(Shape,ManagerFigures):
+    __NAME = "Прямоугольник"
+
+    def __init__(self, x: int, y: int, width: float, height: float):
+        super().__init__(x, y)
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    def info(self):
+        super().info()
+        print(f"Данная фигура: {self.__NAME}"
+              f" c шириной: {self.__width} и высоой: {self.__height}\n")
+
