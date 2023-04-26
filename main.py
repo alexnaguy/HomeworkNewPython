f8d3a100b2993516905b819a0c0180bcc596913d
AlexNaguy <gorniynaguy@mail.ru>
2023-04-26 14:41:59 +0300

    Создал и реализовал класс Car

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+
+class RentCarService:
+    pass
+
+class PrintCarService:
+    pass
+
+class CarInfoService:
+    pass
+class NotificationService:
+    pass
+
+class CarService:
+    pass
+
+class Car():
+    def __init__(self,id:int, brand: str, year: int, color: str, power: int):
+        self.__id = id
+        self.__brand = brand
+        self.__year = year
+        self.__color = color
+        self.__power = power
+
+
+    def __str__(self):
+        return f" ID-номер машины:{self.__id}  марка: {self.__brand} год: {self.__year} цвет: {self.__color}"
+
+
 def execute_application():
     pass
 
