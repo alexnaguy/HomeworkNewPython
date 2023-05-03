6c2d73a59448c72ff90403e283bd94343e4cd535
AlexNaguy <gorniynaguy@mail.ru>
2023-05-03 19:06:12 +0300

    Произвел рефакторинг классов. Вызвал методы в основной программе

--- main.py
+++ main.py
@@ -1,113 +1,104 @@
-
 class RentCarService:
-    def __init__(self, id: int, brand: str, year: int, color: str, power: int):
-
+    def __init__(self, id: int, brand: str, year: int, color: str, power: int, status: bool = False):
         self.__id = id
-        self.__brand = brand
-        self.__year = year
-        self.__color = color
-        self.__power = power
+        self.brand = brand
+        self.year = year
+        self.color = color
+        self.power = power
+        self.status = status
 
+    @property
+    def id(self):
+        return self.__id
 
-    def __str__(self):
-        return f" ID-номер машины:{self.__id}, марка: {self.__brand}, год: {self.__year}," \
-               f" цвет: {self.__color}, мощность {self.__power}"
+    def str(self):
+        return f"ID-номер машины:{self.__id}, марка: {self.brand}, год: {self.year}," \
+               f" цвет: {self.color}, мощность {self.power}, статус автомобиля:{self.status}"
 
 
 class PrintCarService:
-    def __init__(self, id: int,date: str, time: str):
-        self.__id = id
-        self.__date = date
-        self.__time = time
+    def __init__(self, id: int, date: str, time: str):
+        self.id = id
+        self.date = date
+        self.time = time
 
     @staticmethod
-    def print_car( id: int, date : str, time: str):
-        print( f"Заказ на броинрвоание:" \
-               f" Машина по ID-номеру:{id},взята в аренду {date} на время: {time}")
+    def print_car(id: int, date: str, time: str):
+        print(f"Заказ на бронирование:"
+              f" Машина по ID-номеру:{id},взята в аренду {date} на время: {time}")
 
 
 class CarInfoService:
-    def __init__(self, id: int, brand: str, year: int, color: str, power: int):
-        self.__id = id
-        self.__brand = brand
-        self.__year = year
-        self.__color = color
-        self.__power = power
     @staticmethod
     def info_car(id: int, brand: str, year: int, color: str, power: int):
-        print(f" ID-номер автомобиля:{id}, марка: {brand}, год: {year}," \
-               f" цвет: {color}, мощность {power} ")
-
-
+        print(f"Информация о данном автомобиле:\nID-номер автомобиля:{id}, марка: {brand}, год: {year},"
+              f" цвет: {color}, мощность {power} ")
 
 
 class NotificationService:
-    def __int__(self, id: int, brand: str, year: int, color: str, telephone: str):
-        self.__id = id
-        self.__brand = brand
-        self.__year = year
-        self.__color = color
-        self.__telephone = telephone
+    def __init__(self, telephone: str):
+        self.telephone = telephone
 
     @staticmethod
     def send_sms(telephone, brand, year, color):
-        print(f"Смс сообщение отпрвлено на номер абонента {telephone} : "
+        print(f"Смс сообщение отправлено на номер абонента {telephone}: "
               f"Ваш автомобиль {brand}, {year} года, {color} цвета")
 
+
 class CarService:
-    def __init__(self, id: int, brand: str):
-        self.__id = id
-        self.__brand = brand
-    @property
-    def id(self,):
-        return self.__id
-    @property
-    def brand(self):
-        return self.__brand
-    @staticmethod
-    def search_car(name: int, id: int, brand: str):
-        if name == id:
-            print(f"ID-номер {id}, марка {brand}")
+    def __init__(self):
+        self.__list = []
 
+    def add_car(self, car: RentCarService):
+        self.__list.append(car)
 
+    def remove_car(self, car: RentCarService):
+        self.__list.remove(car)
 
+    def find_car_by_id(self, number: int):
+        for element in self.__list:
+            if element.id == number:
+                return element
+        return None
 
-
-
-
-
-
-
+    # def find_car_by_brand_and_year(self, brand: str, year: int):
+    #     for car in self.__list:
+    #         if car.brand == brand and car.year == year:
+    #             return car
+    #     return None
 
 
 def execute_application():
-    list_car = []
-    car1 = RentCarService(10001,"BMW",2018, "red", 220)
-    car2 = RentCarService(10002, "Renault", 2021, "blue", 98)
-    car3 = RentCarService(10003," Skoda", 2015, "black", 150)
-    car4 = RentCarService(10004, "Mersedea", 2023, "red", 170)
-    list_car.append(car1)
-    list_car.append(car2)
-    list_car.append(car3)
-    list_car.append(car4)
-    print(f"Аренда автомобилей,их список: ")
-    for car in list_car:
-        print(car)
+    car_service = CarService()
+    car1 = RentCarService(1001, "Toyota", 2020, "black", 200)
+    car2 = RentCarService(1002, "BMW", 2018, "white", 250)
+    car3 = RentCarService(1003, "Mercedes", 2019, "silver", 220)
+    car_service.add_car(car1)
+    car_service.add_car(car2)
+    car_service.add_car(car3)
 
+    # Поиск автомобиля по ID-номеру
+    car_id = 1003
+    found_car = car_service.find_car_by_id(car_id)
+    if found_car:
+        CarInfoService.info_car(found_car.id, found_car.brand, found_car.year, found_car.color, found_car.power)
+    else:
+        print(f"Автомобиль с ID-номером {car_id} не найден")
 
+    # Бронирование автомобиля
+    car_id = 1001
+    date = "23.06.2023"
+    time = "2 days"
+    booked_car = car_service.find_car_by_id(car_id)
 
-    PrintCarService.print_car(10001,"22.03.2023", "4 дня")
-    NotificationService.send_sms("+7(952) 874 45 68","Mersedes",2023, "red")
-    CarInfoService.info_car(10001,"Renault Megane", 2019, "blue", 98)
-
-
-
-
-
-
-
-
+    if booked_car:
+        #booked_car._RentCarService__status = True
+        PrintCarService.print_car(car_id,date,time)
+        NotificationService.send_sms("8(927)653 77 25", booked_car.brand, booked_car.year, booked_car.color)
+    else:
+        print(f"Автомобиль с ID-номером {car_id} не найден")
 
 
 if __name__ == "__main__":
     execute_application()
+
