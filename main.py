0c273a2691ddac76d72d98c3c6c68f98301a4283
AlexNaguy <gorniynaguy@mail.ru>
2023-05-04 11:54:59 +0300

    Создал и реализовал абсрактный классNotificationService и его подкласссы

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+from abc import ABC, abstractmethod
+class NotificationService:
+    pass
+class NotificationService(ABC):
+    def send_message(self,message: str):
+        pass
 
+
+class EmailNotification(NotificationService):
+    pass
+    def send_message(self,message: str ):
+        print(f"Сообщение {message} отправленно по электронной почте")
+
+
+class MobileNotification(NotificationService):
+    pass
+    def send_message(self,message: str):
+        print(f"Сообщение {message} отправленно на номер телефона")
 
 
 
