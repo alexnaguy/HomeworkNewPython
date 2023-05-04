942e7a9d0e687176ea3f8db70027a32378eaab18
AlexNaguy <gorniynaguy@mail.ru>
2023-05-04 11:56:20 +0300

    Вызвал поочередно методы в основной программе

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from abc import ABC, abstractmethod
-class NotificationService:
-    pass
+
 class NotificationService(ABC):
     def send_message(self,message: str):
         pass
 
 
 class EmailNotification(NotificationService):
-    pass
     def send_message(self,message: str ):
         print(f"Сообщение {message} отправленно по электронной почте")
 
 
 class MobileNotification(NotificationService):
-    pass
     def send_message(self,message: str):
         print(f"Сообщение {message} отправленно на номер телефона")
 
 
 
 def execute_application():
-    pass
+    email = EmailNotification()
+    message = "За вами забронирвоан автомобиль"
+    email.send_message(message)
+
+    phone = MobileNotification()
+    message = "За вами забронирвоан автомобиль"
+    phone.send_message(message)
 
 if __name__ == "__main__":
     execute_application()
