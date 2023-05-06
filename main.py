8e24d970abc39c2a449b95856c18ce86e26080b3
AlexNaguy <gorniynaguy@mail.ru>
2023-05-06 18:59:42 +0300

    Создал и реализовал класс circle

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import math
+
+
+# Задание 1.
+# Создайте класс Circle (окружность). Для данного класса реализуйте ряд
+# перегруженных операторов:
+# Проверка на равенство радиусов двух окружностей (операция ==, !=);
+# Проверка сравнения длин двух окружностей (операции >, <, <=,>=);
+
+class Circle:
+    def __init__(self, radius: float):
+        self.__radius = radius
+        self.__length = 2 * math.pi * self.__radius
+
+    def check_radius(self,radius: float):
+        if radius <= 0:
+            raise InitializationValueError("Значение радиуса должно быть больше 0")
+        return radius
+
+
+
 def execute_application:
     pass
 
