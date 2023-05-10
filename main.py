ac365c4a58b9335b6f8ab0cd7316b1542cf152e1
AlexNaguy <gorniynaguy@mail.ru>
2023-05-10 15:47:39 +0300

    Создал и реализовал абстрактный класс Payments

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+from abc import ABC, abstractmethod
+from copy import copy
+# Рассмотрим принцип разделения интерфейсов.
+# Допустим у нас имеется абстрактный класс Payments и в нем есть три метода:
+# оплата WebMoney, оплата банковской карточкой и оплата по номеру телефона.
+
+class Payments(ABC):
+    @staticmethod
+    @abstractmethod
+    def payWebMoney():
+        pass
+    @staticmethod
+    @abstractmethod
+    def payCreditCard():
+        pass
+    @staticmethod
+    @abstractmethod
+    def payPhoneNumber():
+        pass
+
+
+
+
+
+
+
+
 
 def execute_application:
     pass
