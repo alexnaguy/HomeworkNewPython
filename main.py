2f1ddac06226257904fb2186e413b0666e242049
AlexNaguy <gorniynaguy@mail.ru>
2023-05-26 11:07:00 +0300

    Реализовал вспомогательные  методы по работе с дробями

--- main.py
+++ main.py
@@ -13,7 +13,31 @@ class Fraction:
     def denominator(self):
         return self.__denominator
 
+# +
+    def __is_fraction(self, other):
+        if not isinstance(other,Fraction):
+            raise TypeError(f"Невозможно выполнить сравнение "
+                            f"между типом {self.__class__.__name__} "
+                            f"и {other.__class__.__name__}")
 
+    def __easy_fraction(self, other):
+        if self.__denominator == other.__denominator:
+            new_denominator = self.__denominator
+            new_numerator = self.__numerator + other.__numerator
+            return f"{new_numerator} / {new_denominator}"
+
+    def __int_result(self):
+        """
+        Вернет целое число от деления числителя на знаменатель
+        :param other:
+        :return:
+        """
+        if self.__numerator % self.__denominator == 0:
+            res = self.__numerator / self.__denominator
+            return round(res)
+
+    def __str__(self):
+        return f"{self.__numerator} / {self.__denominator}"
 
 
 
