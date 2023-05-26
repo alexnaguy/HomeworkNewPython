79f7149532f522da0bc4b3c9f5f00c401382202b
AlexNaguy <gorniynaguy@mail.ru>
2023-05-26 11:01:12 +0300

    Cоздал класс Fraction с гетерами и сетерами

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import math
+class Fraction:
+
+    def __init__(self,numerator: int, denominator:int):
+        self.__nod = math.gcd(numerator,denominator)
+        self.__numerator = numerator
+        self.__denominator = denominator
+
+    @property
+    def numerator(self):
+        return self.__numerator
+    @property
+    def denominator(self):
+        return self.__denominator
+
+
+
+
+
 def execute_application():
     pass
 
