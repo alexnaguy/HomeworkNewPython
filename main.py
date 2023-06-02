3b28a08c9ea417e42cd85455457015a741bd82b5
AlexNaguy <gorniynaguy@mail.ru>
2023-06-02 12:06:51 +0300

    Создал класс Calculator с методами переовода СС

--- main.py
+++ main.py
@@ -19,3 +19,31 @@ class Number:
     @base.setter
     def base(self, base):
         self.__base = base
+
+
+class Calculator:
+    def __init__(self,number: Number):
+        self.__number = number
+
+    #Методы пеобразвания в СС
+
+    # Из десятичной в двоичную
+
+    def calc_bin(self):
+        result = bin(self.__number.value)
+        return result
+    # в восьмеричную
+
+    def calc_oct(self):
+        result = oct(self.__number.value)
+        return result
+
+
+    def calc_hex(self):
+        result = hex(self.__number.value)
+        return result
+
+# Из СС любой в десятичную
+    def calc_int(self):
+        result = int(self.__number.value, self.__number.base)
+        return result
