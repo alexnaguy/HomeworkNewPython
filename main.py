e54023cfdd0b6a79621466bbb879eaf3a9379761
AlexNaguy <gorniynaguy@mail.ru>
2023-06-07 10:00:49 +0300

    Вывод результатов на экран и запуск задачи в оснвной программе

--- main.py
+++ main.py
@@ -68,3 +68,21 @@ class JsonPlane:
     def from_json(data):
         obj = json.loads(data)
         return obj
+
+def execute_appliation():
+
+    # Задача 1
+    plane1 = Plane("Boeing 747",70, 170, 430)
+    res = PicklePlane.save_pickle(plane1)
+    print(f"При сериализации объекта Plane через Pickle получилось {res}")
+    obj = PicklePlane.from_pickle(res)
+    print(f"После десериализации объекта получилось {obj}")
+    # Задача 2
+    res = JsonPlane.save_json(plane1)
+    print(f"При сериализации объекта получилось {res}")
+    obj = JsonPlane.from_json(res)
+    print(f"После десериализации объекта через Json получилось {obj}")
+
+
+if __name__ == "__main__":
+    execute_appliation()
