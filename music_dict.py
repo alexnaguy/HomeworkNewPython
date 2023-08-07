36e8cb30a17bd3a3e0d5a68392bf3e915237e885
AlexNaguy <gorniynaguy@mail.ru>
2023-08-07 12:15:36 +0300

    Добавил реаолизацию основной прграммы

--- music_dict.py
+++ music_dict.py
@@ -3,10 +3,12 @@ import json
 def add_data(data_list, artist, song):
     data_list.append({'Исполнитель': artist, 'Песня': song})
 
-def remove_data(data_list, artist, song):
+def remove_data(data_list, song, artist = None ):
     for item in data_list:
         if item['Исполнитель'] == artist or item['Песня'] == song:
             data_list.remove(item)
+        else:
+            "Не нашли"
 
 def save_data(data_list, filename):
     with open(filename, 'w') as file:
