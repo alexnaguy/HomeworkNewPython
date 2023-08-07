2fa13277cf37cf2b437a60325fdc4ec258f31583
AlexNaguy <gorniynaguy@mail.ru>
2023-08-07 11:48:28 +0300

    Добавил методы для словаря

---
+++ music_dict.py
@@ -0,0 +1,18 @@
+import json
+
+def add_data(data_list, artist, song):
+    data_list.append({'Исполнитель': artist, 'Песня': song})
+
+def remove_data(data_list, artist, song):
+    for item in data_list:
+        if item['Исполнитель'] == artist or item['Песня'] == song:
+            data_list.remove(item)
+
+def save_data(data_list, filename):
+    with open(filename, 'w') as file:
+        json.dump(data_list, file)
+
+def load_data(filename):
+    with open(filename, 'r') as file:
+        data_list = json.load(file)
+    return data_list
